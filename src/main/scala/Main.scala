ef3dfbb49e19239bea3d0b39cca3428c09a95060
Thibaut Septon <thibaut.septon@unamur.be>
2022-08-17 16:28:29 +0200

    Refactor code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,48 +1,31 @@
 import com.github.nscala_time.time.Imports.*
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser.JsoupElement
-import net.ruippeixotog.scalascraper.dsl.DSL.*
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract.*
-import net.ruippeixotog.scalascraper.dsl.DSL.Parse.*
-import net.ruippeixotog.scalascraper.model.*
+
 import net.sourceforge.htmlunit.cyberneko.HTMLElements.ElementList
 
 import java.nio.file.{Paths, Files}
 import java.nio.charset.StandardCharsets
 
-@main def main(): Unit = {
+@main def main(): Unit = downloadAndSaveReports()
+
+private def downloadAndSaveReports(): Unit = {
   import scala.language.implicitConversions
   given JsoupBrowser()
+
+  // Scrapping data
   beautifyPrintln("Scrapping...")
-  val nonConcatenatedRecords = allLinksToPages.map(downloadDataFromPage).zipWithIndex.map {
-    case (Right(data), _) => List(data)
-    case (Left(error), i) =>
-      println(f"Error (index $i): $error")
-      Nil
-  }.filter(_.nonEmpty).flatten
-  val concatenatedRecords = nonConcatenatedRecords.foldLeft(Reports.getEmpty) {
-    (acc, report) => acc.copy(report.columns, acc.fields ++ report.fields)
-  }
+  val reports = Scrapper.getReports
+  println("done.")
+
+  // Saving data
   beautifyPrintln("Saving data...")
-  Files.write(Paths.get("./data.csv"), concatenatedRecords.toCSVFormat.getBytes(StandardCharsets.UTF_8))
-}
-
-private def allLinksToPages(using browser: JsoupBrowser): List[String] = {
-  val dataByDateDoc = browser.get("https://nuforc.org/webreports/ndxevent.html")
-  (dataByDateDoc >> elementList("tbody tr td a") >> attr("href")).map(x => f"https://nuforc.org/webreports/${x}")
-}
-
-private def downloadDataFromPage(url: String)(using browser: JsoupBrowser): Either[ReportsError, Reports] = {
-  val dataFromUrlDoc = browser.get(url)
-  val columns: List[JsoupElement] = (dataFromUrlDoc >> elementList("thead tr th font")).map(_.asInstanceOf[JsoupElement])
-  val data: List[JsoupElement] = (dataFromUrlDoc >> elementList("tbody tr")).map(_.asInstanceOf[JsoupElement]) // <tr> tds... </tr>
-  Reports(columns.map(_.innerHtml), data.map(Report.fromJSoup))
+  Files.write(Paths.get("./data.csv"), reports.toCSVFormat.getBytes(StandardCharsets.UTF_8))
+  println("done.")
 }
 
 private def beautifyPrintln(str: String): Unit = println(
-    f"""
-      |${"-" * (str.length*2 +2)}
-      ||${" " * (str.length/2) + str + " " * (str.length/2)}|
-      |${"-" * (str.length*2 +2)}
-    """.stripMargin
-  )
+  f"""
+      |${"-" * (str.length * 2 + 2)}
+      ||${" " * (str.length / 2) + str + " " * (str.length / 2)}|
+      |${"-" * (str.length * 2 + 2)}""".stripMargin
+)
---
+++ src/main/scala/Scrapper.scala
@@ -0,0 +1,46 @@
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser.JsoupElement
+import net.ruippeixotog.scalascraper.dsl.DSL.*
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract.*
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse.*
+import net.ruippeixotog.scalascraper.model.*
+
+object Scrapper {
+
+  def getReports(using browser: JsoupBrowser): Reports = {
+    val nonConcatenatedRecords = allPages
+      .map(downloadDataFromPage)
+      .zipWithIndex
+      .map {
+        case (Right(data), _) => List(data)
+        case (Left(error), i) =>
+          println(f"Error (index $i): $error")
+          Nil
+      }
+      .filter(_.nonEmpty)
+      .flatten
+    val concatenatedRecords: Reports =
+      nonConcatenatedRecords.foldLeft(Reports.getEmpty) { (acc, report) =>
+        acc.copy(report.columns, acc.fields ++ report.fields)
+      }
+    concatenatedRecords
+  }
+
+  private def allPages(using browser: JsoupBrowser): List[String] = {
+    val dataByDateDoc = browser.get("https://nuforc.org/webreports/ndxevent.html")
+    (dataByDateDoc >> elementList("tbody tr td a") >> attr("href")).map(x =>
+      f"https://nuforc.org/webreports/${x}"
+    )
+  }
+
+  private def downloadDataFromPage(
+      url: String
+  )(using browser: JsoupBrowser): Either[ReportsError, Reports] = {
+    val dataFromUrlDoc = browser.get(url)
+    val columns: List[JsoupElement] =
+      (dataFromUrlDoc >> elementList("thead tr th font")).map(_.asInstanceOf[JsoupElement])
+    val data: List[JsoupElement] =
+      (dataFromUrlDoc >> elementList("tbody tr")).map(_.asInstanceOf[JsoupElement])
+    Reports(columns.map(_.innerHtml), data.map(Report.fromJSoup))
+  }
+}
